3ad798ced13cf6ae7b6705477eecca27f7eb9f02
Mateusz Sapala <sapala.mateusz@gmail.com>
2021-08-24 14:54:45 +0200

    Create project

---
+++ Hibernate-with-Spring-Boot/src/main/java/com/sapalamateusz/HibernatewithSpringBoot/HibernateWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sapalamateusz.HibernatewithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ Hibernate-with-Spring-Boot/src/test/java/com/sapalamateusz/HibernatewithSpringBoot/HibernateWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapalamateusz.HibernatewithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
